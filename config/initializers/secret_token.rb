bb816689eba155c23a67e26b842755c648411587
Li Jiaming <jiamli@paypal.com>
2012-10-27 21:11:33 +0800

    Init add backbone js

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneOnRails::Application.config.secret_token = 'aa75559aab94c142a8aa47e6b192b78367701474719a4b20cd69b4db6e5e6232a55a4a0b28caa58a7efc6ee5831d3a807f2eab8659bcf0f931030c43cb320835'
